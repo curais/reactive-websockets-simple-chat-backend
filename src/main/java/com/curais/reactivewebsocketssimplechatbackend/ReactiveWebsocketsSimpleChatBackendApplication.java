0127f8984cf1b18ddd873d07af06a5ed9acd37a3
curais <craisochoa22@gmail.com>
2020-12-23 13:01:56 -0700

    first commit

---
+++ src/main/java/com/curais/reactivewebsocketssimplechatbackend/ReactiveWebsocketsSimpleChatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.curais.reactivewebsocketssimplechatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveWebsocketsSimpleChatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveWebsocketsSimpleChatBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curais/reactivewebsocketssimplechatbackend/ReactiveWebsocketsSimpleChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curais.reactivewebsocketssimplechatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveWebsocketsSimpleChatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
